a16bdbfbcc45ae019e99fa04dc0a7b99ee38d1f0
Cristina <avicon95@protonmail.com>
2024-06-20 02:30:07 -0500

    fix: eye password show on and off

--- app/login.tsx
+++ app/login.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Image, TextInput, View, Text } from "react-native";
+import { Image, TextInput, View, Text, TouchableOpacity } from "react-native";
 import { router } from "expo-router";
 import { useSession } from "../context/ctx";
 import React from "react";
@@ -11,6 +11,7 @@ export default function Login() {
 
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
 
   const handleLogin = async () => {
     signIn(email, password);
@@ -36,14 +37,28 @@ export default function Login() {
         className="w-full p-4 border-yellow border-[1rem] rounded-lg mb-4"
         placeholder="Correo Electrónico"
         keyboardType="email-address"
+        autoCapitalize="none"
         onChangeText={(text) => setEmail(text)}
       />
-      <TextInput
-        className="w-full p-4 border-yellow border-[1rem] rounded-lg mb-2"
-        placeholder="Contraseña"
-        secureTextEntry
-        onChangeText={(text) => setPassword(text)}
-      />
+      <View className="w-full flex flex-row items-center border-yellow border-[1rem] rounded-lg mb-2">
+        <TextInput
+          className="flex-1 p-4"
+          placeholder="Contraseña"
+          autoCapitalize="none"
+          secureTextEntry={!passwordVisible}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <TouchableOpacity
+          className="pr-4"
+          onPress={() => setPasswordVisible(!passwordVisible)}
+        >
+          <Ionicons
+            name={passwordVisible ? "eye-off" : "eye"}
+            size={24}
+            color="black"
+          />
+        </TouchableOpacity>
+      </View>
       <Text className="text-xs font-light mb-4">¿Olvidó la contraseña?</Text>
       <CustomButton title="Ingresar" onPress={handleLogin}></CustomButton>
       <Text className="my-4">ó</Text>
